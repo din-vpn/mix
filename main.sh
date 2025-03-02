bfa7772f4a89f3b86ea73ad38df4d9b1a0bda413
din-vpn <din71971@gmail.com>
2025-03-03 03:13:54 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -31,7 +31,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
-echo -e "\033[96;1m                        DINN_STORE VPN             \033[0m"
+echo -e "\033[96;1m                        CILACAP_TUNNELING             \033[0m"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 sleep 3
